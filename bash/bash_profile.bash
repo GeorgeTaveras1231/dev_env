077c45fcdc9c181a75ce24db6703ae96c11e30f6
George Taveras <gtaveras@xogrp.com>
2018-03-05 22:01:58 -0500

    Add bash_completion

--- bash/bash_profile.bash
+++ bash/bash_profile.bash
@@ -1,2 +1,7 @@
 [ -z "$TMUX" ] && export TERM=screen-256color
 export PS1=" \[\033[38;5;5m\]\$\[\033[0m\] "
+
+# Git autocomplete
+if [ -f $(brew --prefix)/etc/bash_completion ]; then
+  . $(brew --prefix)/etc/bash_completion
+fi
